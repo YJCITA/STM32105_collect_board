403cf6c5cb35eb2b5dc7b187f4c1d6f608215a8f
YJCITA <yujinwpu@163.com>
2016-09-02 14:17:04 +0800

    remove some useless files

--- MDK/Obj/core_cm3.d
+++
@@ -1,2 +0,0 @@
-.\Obj\core_cm3.o: ..\LIB\CMSIS\CM3\CoreSupport\core_cm3.c
-.\Obj\core_cm3.o: D:\Keil\ARM\RV31\INC\stdint.h
--- MDK/Obj/startup_stm32f10x_cl.d
+++
@@ -1,1 +0,0 @@
-.\Obj\startup_stm32f10x_cl.o: ..\LIB\CMSIS\CM3\DeviceSupport\ST\STM32F10x\startup\arm\startup_stm32f10x_cl.s
